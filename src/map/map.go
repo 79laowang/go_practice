14d8f188f4eb4d52215aedb3319536a4b502f7ac
79laowang <edward.wang@163.com>
2019-12-12 23:45:15 +0800

    map usage samples

--- src/map/map.go
+++ src/map/map.go
@@ -13,6 +13,55 @@ func main() {
         personSalary["Lili"] = 18000
         personSalary["Ke"] = 30000
         fmt.Println("personSalary map contents:", personSalary)
-
     }
+
+    fmt.Println("Accessing items of a map:")
+    workerSalary := map[string]int{
+        "steve": 12000,
+        "jamie": 15000,
+    }
+    workerSalary["mike"] = 9000
+    employee := "jamie"
+    fmt.Println("Salary of", employee, "is", workerSalary[employee])
+    // if we try to access an element which is not present then, the zero value of int which is 0 will be returned. 
+    fmt.Println("Salary of joe is", workerSalary["joe"])
+
+    fmt.Println("Checking whether a key is present in a map or not.")
+    newEmp := "joe"
+    value, ok := personSalary[newEmp]
+    if ok == true {
+        fmt.Println("Salary of", newEmp, "is", value)
+    } else {
+        fmt.Println(newEmp, "Not found")
+    }
+
+    fmt.Println("All items of a map:")
+    for key, value := range workerSalary {
+        fmt.Printf("workerSalary[%s] = %d\n", key, value)
+    }
+
+    fmt.Println("Deleting a item from map:")
+    delete(workerSalary, "mike")
+    fmt.Println("map after deletion", workerSalary)
+
+    fmt.Println("length of map is:", len(workerSalary))
+
+    fmt.Println("Maps are reference types:")
+    workerSalary["mike"] = 9000
+    fmt.Println("Original worker salary:", workerSalary)
+    newPersonSalary := workerSalary
+    newPersonSalary["mike"] = 18000
+    fmt.Println("Worker salary changed from reference map:", workerSalary)
+
+    fmt.Println("Maps equality:")
+    map1 := map[string]int{
+        "one": 1,
+        "two": 2,
+    }
+    var map2 map[string]int
+    if map2 == nil {
+        fmt.Println("map is empty!")
+        map2 = map1
+    }
+    fmt.Println("map2:", map2, "map1:", map1)
 }
