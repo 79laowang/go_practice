e91167bec5a365abf631a55599d5e4de2e2dd226
79laowang <edward.wang@163.com>
2019-12-30 00:23:25 +0800

    Specified part values of structure

--- src/structure/structure.go
+++ src/structure/structure.go
@@ -23,4 +23,27 @@ func main() {
 
     fmt.Println("Employee 1:", emp1)
     fmt.Println("Employee 2:", emp2)
+
+    fmt.Println("Creating anonymous structures:")
+    emp3 := struct {
+        firstName, lastName string
+        age, salary         int
+    }{
+        firstName: "Andreah",
+        lastName:  "Nikola",
+        age:       31,
+        salary:    5000,
+    }
+    fmt.Println("Employee 3", emp3)
+
+    fmt.Println("Zero value of structure:")
+    var emp4 Employee // zero valued structure
+    fmt.Println("Employee 4:", emp4)
+
+    emp5 := Employee{
+        firstName: "Jhon",
+        lastName: "Paul",
+    }
+    fmt.Println("Specify part values of structure:")
+    fmt.Println("Employee 5", emp5)
 }
