80d9753f0ff5296c9c1067bd1bf99c5ee23ecaeb
79laowang <edward.wang@163.com>
2020-01-05 23:27:36 +0800

    Pointer Receivers vs Value Receivers

--- src/method/method.go
+++ src/method/method.go
@@ -34,6 +34,20 @@ func (c Circle) Area() float64 {
     return math.Pi * c.radius * c.radius
 }
 
+/*
+Method with value receiver
+*/
+func (e Employee) changeName(newName string) {
+    e.name = newName
+}
+
+/*
+Method with pointer receiver
+*/
+func (e *Employee) changeAge(newAge int) {
+    e.salary = newAge
+}
+
 func main() {
     emp1 := Employee {
         name:    "Sam Adolf",
@@ -51,4 +65,21 @@ func main() {
         radius: 12,
     }
     fmt.Printf("\nArea of circle: %f", c.Area())
+
+    fmt.Println("\nPointer Receivers vs Value Receivers:")
+    e := Employee{
+        name: "Makr Andrew",
+        salary: 5000,
+        currency: "$",
+    }
+    fmt.Printf("Employee name before change:%s", e.name)
+    e.changeName("Michael Andrew")
+    fmt.Printf("\nEmployee name after change: %s", e.name)
+
+    fmt.Printf("\n\nEmployee salary before change: %d", e.salary)
+    (&e).changeAge(5001)
+    fmt.Printf("\nEmployee salary after change: %d", e.salary)
+    e.changeAge(5002)
+    fmt.Printf("\nEmployee salary after change: %d", e.salary)
+
 }
