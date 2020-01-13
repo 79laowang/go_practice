836accfec927228ce3b6c80d8469e6f19f2e26f6
79laowang <edward.wang@163.com>
2020-01-13 23:10:16 +0800

    Practical use of interface

--- src/interface/interface.go
+++ src/interface/interface.go
@@ -11,8 +11,8 @@ type VowelsFinder interface {
 
 type MyString string
 
-// MyString implements VowelsFinder
-func (ms MyString) FindVowels() []rune {
+// MyString which is receiver implements VowelsFinder
+func (ms MyString) FindVowels() []rune { // This is a method
     var vowels []rune
     for _, rune := range ms {
         if rune == 'a' || rune == 'e' || rune == 'i' || rune == 'o' || rune == 'u' {
@@ -22,9 +22,54 @@ func (ms MyString) FindVowels() []rune {
     return vowels
 }
 
+type SalaryCalculator interface {
+    CalculateSalary() int
+}
+
+type Permanent struct {
+    empId     int
+    basicpay  int
+    pf        int
+}
+
+type Contract struct {
+    empId     int
+    basicpay  int
+}
+
+//Salary of permanent employee is sum of basic pay and pf
+func (p Permanent) CalculateSalary() int {
+    return p.basicpay + p.pf
+}
+
+//Salary of contract employee is the basic pay alone
+func (c Contract) CalculateSalary() int {
+    return c.basicpay
+}
+
+/*
+total expense is calculated by interating though the SalaryCalculator slice and summing the salaries of the individual employees
+*/
+func totalExpense(s []SalaryCalculator) {
+    expense := 0
+    for _, v := range s {
+        expense = expense + v.CalculateSalary()
+    }
+    fmt.Printf("Total Expense Per Month $%d", expense)
+}
+
 func main() {
     name := MyString("Sam Anderson")
     var v VowelsFinder
+    // name is a receiver with a method FindVowels, so name is implemention  of
+    // interface VowelsFinder
     v = name // possible since MyString implements VowelsFinder
     fmt.Printf("Vowels are %c", v.FindVowels())
+
+    fmt.Println("\n\nPractical use of interface,")
+    pemp1 := Permanent{1, 5000, 20}
+    pemp2 := Permanent{2, 6000, 30}
+    cemp1 := Contract{3, 3000}
+    employees := []SalaryCalculator{pemp1, pemp2, cemp1}
+    totalExpense(employees)
 }
