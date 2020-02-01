75ca20f07f2bd96e231e38ca46c1ee13fd0b6240
79laowang <edward.wang@163.com>
2020-02-01 12:31:14 +0800

    channel usage samples

---
+++ src/channel/channel.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+)
+
+func hello(done chan bool) {
+	fmt.Println("\nHello world goroutine with channel")
+	done <- true // write to channel
+}
+
+func calcSquares(number int, squareop chan int) {
+	sum := 0
+	for number != 0 {
+		digit := number % 10
+		sum += digit * digit
+		number /= 10
+	}
+	squareop <- sum
+}
+
+func calcCubes(number int, cubeop chan int) {
+	sum := 0
+	for number != 0 {
+		digit := number % 10
+		sum += digit * digit * digit
+		number /= 10
+	}
+	cubeop <- sum
+}
+
+func main() {
+	var a chan int
+	if a == nil {
+		fmt.Println("channel a is nil, going to define it")
+		a = make(chan int)
+		fmt.Printf("Type of a is %T", a)
+	}
+
+	done := make(chan bool)
+	go hello(done)
+	<-done // read from channel
+	fmt.Println("main function")
+
+	number := 589
+	sqrch := make(chan int)
+	cubech := make(chan int)
+	go calcSquares(number, sqrch)
+	go calcCubes(number, cubech)
+	squares, cubes := <-sqrch, <-cubech
+	fmt.Println("Final output", squares+cubes)
+}
