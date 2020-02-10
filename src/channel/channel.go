5f628f3589c28d6eb950a5707de44445f79bc31d
79laowang <edward.wang@163.com>
2020-02-10 23:36:17 +0800

    add channel examples

--- src/channel/channel.go
+++ src/channel/channel.go
@@ -34,6 +34,42 @@ func sendData(sendch chan<- int) {
 	sendch <- 10
 }
 
+func producer(chn1 chan int) {
+	for i := 0; i < 10; i++ {
+		chn1 <- i
+	}
+	close(chn1)
+}
+
+func digits(number int, dchn1 chan int) {
+	for number != 0 {
+		digit := number % 10
+		dchn1 <- digit
+		number /= 10
+	}
+	close(dchn1)
+}
+
+func calcSquares1(number int, squareop chan int) {
+	sum := 0
+	dch := make(chan int)
+	go digits(number, dch)
+	for digit := range dch {
+		sum += digit * digit
+	}
+	squareop <- sum
+}
+
+func calcCubes1(number int, cubeop chan int) {
+	sum := 0
+	dch := make(chan int)
+	go digits(number, dch)
+	for digit := range dch {
+		sum += digit * digit * digit
+	}
+	cubeop <- sum
+}
+
 func main() {
 	var a chan int
 	if a == nil {
@@ -59,4 +95,28 @@ func main() {
 	chn1 := make(chan int)
 	go sendData(chn1)
 	fmt.Println("read from channel:", <-chn1)
+
+	ch := make(chan int)
+	go producer(ch)
+	for {
+		v, ok := <-ch
+		if ok == false {
+			break
+		}
+		fmt.Println("Received from channel:", v, ok)
+	}
+
+	ch1 := make(chan int)
+	go producer(ch1)
+	for v1 := range ch1 {
+		fmt.Println("Received from channel using range loop:", v1)
+	}
+
+	sqrch1 := make(chan int)
+	cubech1 := make(chan int)
+	go calcSquares1(number, sqrch1)
+	go calcCubes1(number, cubech1)
+	squares1, cubes1 := <-sqrch1, <-cubech1
+	fmt.Println("Final output", squares1+cubes1)
+
 }
