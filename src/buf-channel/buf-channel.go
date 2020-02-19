7c2149f47548e90e7144d77b6d8bd7582161b045
79laowang <edward.wang@163.com>
2020-02-20 00:36:22 +0800

    example for writes to a buffered channel block

--- src/buf-channel/buf-channel.go
+++ src/buf-channel/buf-channel.go
@@ -1,6 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func write(ch chan int) {
+	for i := 0; i < 5; i++ {
+		ch <- i
+		fmt.Println("successfully wrote", i, "to ch")
+	}
+	close(ch)
+}
 
 func main() {
 	ch := make(chan string, 2)
@@ -8,4 +19,12 @@ func main() {
 	ch <- "Wang"
 	fmt.Println(<-ch)
 	fmt.Println(<-ch)
+
+	ch1 := make(chan int, 2)
+	go write(ch1)
+	time.Sleep(2 * time.Second)
+	for v := range ch1 {
+		fmt.Println("read value", v, "from ch1")
+		time.Sleep(2 * time.Second)
+	}
 }
