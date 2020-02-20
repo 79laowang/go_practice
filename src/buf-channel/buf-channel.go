7b3f78a9da6cdcdec28149b61b865b80b7723685
79laowang <edward.wang@163.com>
2020-02-21 00:50:35 +0800

    dead lock for buffer channel

--- src/buf-channel/buf-channel.go
+++ src/buf-channel/buf-channel.go
@@ -27,4 +27,14 @@ func main() {
 		fmt.Println("read value", v, "from ch1")
 		time.Sleep(2 * time.Second)
 	}
+
+	fmt.Println("Dead lock:")
+	ch2 := make(chan string, 2)
+	ch2 <- "naveen"
+	ch2 <- "paul"
+	ch2 <- "bob"
+	// write 3 strings to a buffered channel of capacity 2, the third write is blocked since the channel has exceeded its capacity
+	// no concurrent routine reading from this channel. Hence there will be a deadlock and the program will panic at run time
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
 }
