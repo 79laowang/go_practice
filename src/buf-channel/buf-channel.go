0ece48f2f8c74b64dd02748fde626a8b595b1c59
79laowang <edward.wang@163.com>
2020-03-01 23:50:46 +0800

    work pool part one

--- src/buf-channel/buf-channel.go
+++ src/buf-channel/buf-channel.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 	"sync"
 )
@@ -19,10 +20,26 @@ func process(i int, wg *sync.WaitGroup) {
 	fmt.Println("started Gorouting", i)
 	time.Sleep(2 * time.Second)
 	fmt.Printf("Goroutine %d ended\n", i)
-	wg.Done()
+	wg.Done() // The counter is decremented every call
 }
 
+type Job struct {
+	id         int
+	randomno   int
+}
+
+type Result struct {
+	job          Job
+	sumofdigits  int
+}
+
+var jobs = make(chan Job, 10)
+var results = make(chan Result, 10)
+
+
 func main() {
+	fmt.Println("\nBuffered channels:")
+	//Buffered channels can be created by passing an additional capacity parameter to the make function which specifies the size of the buffer.
 	ch := make(chan string, 2)
 	ch <- "Bob"
 	ch <- "Wang"
@@ -47,7 +64,7 @@ func main() {
 	fmt.Println(<-ch2)
 	fmt.Println(<-ch2)
 
-	fmt.Println("Length vs Capacity:")
+	fmt.Println("\nLength vs Capacity:")
 	ch3 := make(chan string, 3)
 	ch3 <- "naveen"
 	ch3 <- "bob"
@@ -56,14 +73,23 @@ func main() {
 	fmt.Println("read value", <-ch3)
 	fmt.Println("new length is", len(ch3))
 	
-	fmt.Println("WaitGourp:")
+	fmt.Println("\nWaitGourp:")
 	no := 3
 	var wg sync.WaitGroup
+	// the WaitGroup's counter is incremented by add method
 	for i := 0; i < no; i++ {
+		fmt.Println("Increase one go routine", i + 1)
 		wg.Add(1)
 		go process(i, &wg)
 	}
+	// This makes the main Goroutine to wait until the counter becomes zero
 	wg.Wait()
 	fmt.Println("All go routines finished execting!")
+
+	/*
+	In general, a worker pool is a collection of threads which are waiting for tasks to be assigned to them. 
+	Once they finish the task assigned, they make themselves available again for the next task.
+	*/
+	fmt.Println("\nWorker Pool:")
 
 }
