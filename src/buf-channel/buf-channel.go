7fda7eab22b0dcb97aaea9556c5c30336aa36b53
79laowang <edward.wang@163.com>
2020-03-02 22:13:45 +0800

    Worker Pool Implementation

--- src/buf-channel/buf-channel.go
+++ src/buf-channel/buf-channel.go
@@ -36,6 +36,51 @@ type Result struct {
 var jobs = make(chan Job, 10)
 var results = make(chan Result, 10)
 
+func digits(number int) int {
+	sum := 0
+	no := number
+	for no != 0 {
+		digit := no % 10
+		sum += digit
+		no /= 10
+	}
+	time.Sleep(2 * time.Second)
+	return sum
+}
+
+func worker(wg *sync.WaitGroup) {
+	for job := range jobs {
+		output := Result{job, digits(job.randomno)}
+		results <- output
+	}
+	wg.Done()
+}
+
+func createWorkerPool(noOfWorkers int) {
+	var wg sync.WaitGroup
+	for i := 0; i < noOfWorkers; i++ {
+		wg.Add(1)
+		go worker(&wg)
+	}
+	wg.Wait()
+	close(results)
+}
+
+func allocate(noOfJobs int) {
+	for i := 0; i < noOfJobs; i++ {
+		randomno := rand.Intn(999)
+		job := Job{i, randomno}
+		jobs <- job
+	}
+	close(jobs)
+}
+
+func result(done chan bool) {
+	for result := range results {
+		fmt.Printf("Job id %d, input random no %d, sum of digits %d\n", result.job.id, result.job.randomno, result.sumofdigits)
+	}
+	done <- true
+}
 
 func main() {
 	fmt.Println("\nBuffered channels:")
@@ -90,6 +135,20 @@ func main() {
 	In general, a worker pool is a collection of threads which are waiting for tasks to be assigned to them. 
 	Once they finish the task assigned, they make themselves available again for the next task.
 	*/
+    fmt.Println("-----------------------------------")
 	fmt.Println("\nWorker Pool:")
+	startTime := time.Now()
+	noOfJobs := 100
+	go allocate(noOfJobs)
+	done := make(chan bool)
+	go result(done)
+	noOfWorkers := 10
+	createWorkerPool(noOfWorkers)
+	<-done
+	endTime := time.Now()
+	diff := endTime.Sub(startTime)
+	fmt.Println("Total time taken:", diff.Seconds(), "seconds")
+
+	
 
 }
