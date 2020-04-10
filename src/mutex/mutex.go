d7b5ca54fd4ca3dfdbd856a64e9a41e09907385a
79laowang <edward.wang@163.com>
2020-04-11 00:26:21 +0800

    add usage in either use Mutex or Channel

--- src/mutex/mutex.go
+++ src/mutex/mutex.go
@@ -1,5 +1,7 @@
 package main
 
+// In general use channels when Goroutines need to communicate with each other and mutexes when only one Goroutine should access the critical section of code.
+
 import (
 	"fmt"
 	"sync"
